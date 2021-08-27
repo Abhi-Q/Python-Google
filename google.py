1d1f72c9d8d1900081f96d97f0045a761a939edd
Abhinav <abhinav.ranish@outlook.com>
2021-08-27 19:50:35 +0300

    Add files via upload

---
+++ google.py
@@ -0,0 +1,12 @@
+import pywhatkit
+import os
+from os import system
+os.system("title Abhinav uses google.")
+os.system("color 0a")
+os.system("cls")
+while True:
+    search = input("What do you want to google?\n")
+    print("Pinging google.com \nHacking its mainframe. \nRetreving Answers.")
+    pywhatkit.search(search)
+    input("Press Enter to continue...\n")
+    os.system("cls")
